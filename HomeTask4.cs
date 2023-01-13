7e5701bef3f14d681460dbb7918e33f207b212e4
WhiteBorshek <121310443+WhiteBorshek@users.noreply.github.com>
2023-01-14 02:12:56 +0500

    Add files via upload

---
+++ HomeTask4.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+int i = 1;
+
+while (i <= number)
+{
+if (i % 2 != 1)
+ Console.Write("{0} ", i);
+++i;
+}
